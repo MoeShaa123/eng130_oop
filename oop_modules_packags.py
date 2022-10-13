b8e3e3924825c0f7640293ab6f30e328562ee921
MoeShaa123 <mohamedyusuf01@hotmail.com>
2022-10-13 12:06:26 +0100

    first commit

---
+++ oop_modules_packags.py
@@ -0,0 +1,9 @@
+
+# Luck draw
+# keyword called import
+
+# There are many built-in modules that can be used in python
+import random
+
+print(random.random())
+# each time it's run it generates a random number
