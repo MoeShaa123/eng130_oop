81641cb8b23528b77d07c1684ebb8489431bcc18
MoeShaa123 <mohamedyusuf01@hotmail.com>
2022-10-13 12:32:11 +0100

    first commit

--- oop_modules_packags.py
+++ oop_modules_packags.py
@@ -3,7 +3,18 @@
 # keyword called import
 
 # There are many built-in modules that can be used in python
-import random
+from random import *
+import math
+# print(random())
+# each time it's run it generates a random number from 0-1
 
-print(random.random())
-# each time it's run it generates a random number
+# calculate DOB or year of birth
+number = 23.66
+# any numbers - to round the figure up
+# number 1.5 or above to round up to 2
+# number 1.49 or less round down to 1
+
+print(math.ceil(number))  # ceil will help round up the number
+print(math.floor(number)) # floor will help round down the number
+
+
